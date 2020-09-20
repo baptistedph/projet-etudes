83942362051a1de3140d0e4e51deeb18ce4fcf67
Baptiste DAUPHOUY <57500708+baptistedph@users.noreply.github.com>
2020-09-20 19:26:32 +0200

    Add files via upload

---
+++ js/gauge.js
@@ -0,0 +1,6 @@
+const gauge = document.querySelector('.gauge')
+
+window.addEventListener('scroll', () => {
+  const progress = window.pageYOffset / (document.body.scrollHeight - window.innerHeight) * 100
+  gauge.style.width = progress + '%'
+})
