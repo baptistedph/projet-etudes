6e933341e58bf3eb50df90c0a33f4f88269c96e1
BaptouFou <baptoufouu@gmail.com>
2020-10-04 16:53:34 +0200

    Tables added

--- js/gauge.js
+++ js/gauge.js
@@ -2,5 +2,5 @@ const gauge = document.querySelector('.gauge')
 
 window.addEventListener('scroll', () => {
   const progress = window.pageYOffset / (document.body.scrollHeight - window.innerHeight) * 100
-  gauge.style.width = progress + '%'
+  gauge.style.width = `${progress}%`
 })
